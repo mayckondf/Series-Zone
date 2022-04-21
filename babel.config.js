064805e06a6b7f9d6466e5887263bb3b687fa26f
Mayckon DF <mayckon.fr@gmail.com>
2022-04-20 21:11:20 -0300

    chore: change content folder chore: add path aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '@src': './src/',
+          '@components': './src/components/',
+        },
+      },
+    ],
+  ],
 };
