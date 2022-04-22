0d4519fdbaaebd0052a0f91fb6103c83c518251b
Mayckon DF <mayckon.fr@gmail.com>
2022-04-22 20:04:07 -0300

    wip: search page

---
+++ src/pages/Search/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import TabPageContainer from '@src/components/TabPageContainer';
+
+const Search: React.FC = () => {
+  return <TabPageContainer />;
+};
+
+export default Search;
