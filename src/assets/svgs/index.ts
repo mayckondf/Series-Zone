d658362d299a7392229e298b6b817748f1e2be30
Mayckon DF <mayckon.fr@gmail.com>
2022-04-22 19:24:46 -0300

    chore: add screen icons svg

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,5 +1,11 @@
+import Favorite from './favorite.svg';
+import Home from './home.svg';
 import Logo from './logo.svg';
+import Search from './search.svg';
 
 export const svgs = {
   logo: Logo,
+  home: Home,
+  search: Search,
+  favorite: Favorite,
 };
