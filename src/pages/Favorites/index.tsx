195bfd1ff867971e495d2aad6dbbda023a3d0398
Mayckon DF <mayckon.fr@gmail.com>
2022-04-22 20:03:57 -0300

    wip: favorite page

---
+++ src/pages/Favorites/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import TabPageContainer from '@src/components/TabPageContainer';
+
+const Favorites: React.FC = () => {
+  return <TabPageContainer />;
+};
+
+export default Favorites;
